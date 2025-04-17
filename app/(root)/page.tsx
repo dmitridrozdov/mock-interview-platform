3a3be48988ec7a5eaeeb26118b28a0e0175089cf
dmitridrozdov <dm.drozdov@gmail.com>
2025-04-17 13:18:49 +1000

    Update page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,5 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
@@ -8,6 +10,13 @@ const page = () => {
           <h2>
             Get Interview Ready with <span className="text-primary">AI-Powered</span> Practice & Feedback
           </h2>
+          <p className='text-lg'>
+            Practice coding interviews with our AI-powered platform. Get instant feedback and improve your skills.
+            Join now and take your coding interviews to the next level!
+          </p>
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Start an Interview</Link>
+          </Button>
         </div>
       </section>
     </>

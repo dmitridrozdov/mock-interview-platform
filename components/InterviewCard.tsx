98690f5e837b79e43fee70ea8f3c36b9cf8fb756
dmitridrozdov <dm.drozdov@gmail.com>
2025-04-22 16:21:16 +1000

    Update InterviewCard.tsx

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -1,11 +1,34 @@
 import dayjs from "dayjs"
+import { cn } from "@/lib/utils";
 
 const InterviewCard = ({ interviewId, userId, role, type, techstack, createdAt }: InterviewCardProps) => {
     const feedback = null as Feedback | null
     const normalizedType = /mix/gi.test(type) ? "Mixed" : type
     const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format("MMMM D, YYYY")
+
+    const badgeColor =
+    {
+      Behavioral: "bg-light-400",
+      Mixed: "bg-light-600",
+      Technical: "bg-light-800",
+    }[normalizedType] || "bg-light-600";
+
   return (
-    <div>InterviewCard</div>
+    <div className="card-border w-[360px] max-sm:w-full min-h-96">
+        <div className="card-interview">
+            <div>
+                {/* Type Badge */}
+                <div
+                    className={cn(
+                    "absolute top-0 right-0 w-fit px-4 py-2 rounded-bl-lg",
+                    badgeColor
+                    )}
+                >
+                    <p className="badge-text ">{normalizedType}</p>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 

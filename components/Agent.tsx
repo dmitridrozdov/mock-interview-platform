442d219cf7d551726d0199932c0c4a58b7f2e033
dmitridrozdov <dm.drozdov@gmail.com>
2025-05-06 18:21:44 +1000

    Update Agent.tsx

---
+++ components/Agent.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Agent = () => {
+  return (
+    <div className="call-view">
+        <div className="card-interviewer">
+            <div className="avatar">
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Agent

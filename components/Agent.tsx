815b8caa31c9d2e65cc21688cb32053f70a7d58e
dmitridrozdov <dm.drozdov@gmail.com>
2025-05-07 15:48:25 +1000

    Update Agent.tsx

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -20,6 +20,20 @@ const Agent = () => {
             </div>
             <h3>AI Interviewer</h3>
         </div>
+
+        {/* User Profile Card */}
+        <div className="card-border">
+          <div className="card-content">
+            <Image
+              src="/user-avatar.png"
+              alt="profile-image"
+              width={539}
+              height={539}
+              className="rounded-full object-cover size-[120px]"
+            />
+            <h3>!!userName!!</h3>
+          </div>
+        </div>
     </div>
   )
 }

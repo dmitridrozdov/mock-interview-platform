f8d9b4e8fa9754a608a7812ed4c88d83463184e6
dmitridrozdov <dm.drozdov@gmail.com>
2025-05-07 15:54:16 +1000

    Update Agent.tsx

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -1,40 +1,42 @@
 import React from 'react'
 import Image from 'next/image'
 
-const Agent = () => {
+const Agent = ({ userName }: AgentProps) => {
 
     const isSpeaking = true;
 
   return (
-    <div className="call-view">
-        <div className="card-interviewer">
-            <div className="avatar">
-                <Image
-                    src="/ai-avatar.png"
-                    alt="profile-image"
-                    width={65}
-                    height={54}
-                    className="object-cover"
-                />
-                {isSpeaking && <span className="animate-speak" />}
+    <>
+        <div className="call-view">
+            <div className="card-interviewer">
+                <div className="avatar">
+                    <Image
+                        src="/ai-avatar.png"
+                        alt="profile-image"
+                        width={65}
+                        height={54}
+                        className="object-cover"
+                    />
+                    {isSpeaking && <span className="animate-speak" />}
+                </div>
+                <h3>AI Interviewer</h3>
             </div>
-            <h3>AI Interviewer</h3>
-        </div>
 
-        {/* User Profile Card */}
-        <div className="card-border">
-          <div className="card-content">
-            <Image
-              src="/user-avatar.png"
-              alt="profile-image"
-              width={539}
-              height={539}
-              className="rounded-full object-cover size-[120px]"
-            />
-            <h3>!!userName!!</h3>
-          </div>
+            {/* User Profile Card */}
+            <div className="card-border">
+            <div className="card-content">
+                <Image
+                src="/user-avatar.png"
+                alt="profile-image"
+                width={539}
+                height={539}
+                className="rounded-full object-cover size-[120px]"
+                />
+                <h3>{userName}</h3>
+            </div>
+            </div>
         </div>
-    </div>
+    </>
   )
 }
 

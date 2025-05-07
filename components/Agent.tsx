d21ba7c0dd0d57776356bb9f1508201de7d3edd0
dmitridrozdov <dm.drozdov@gmail.com>
2025-05-07 15:37:02 +1000

    Update Agent.tsx

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Agent = () => {
   return (
     <div className="call-view">
         <div className="card-interviewer">
             <div className="avatar">
+                <Image
+                    src="/ai-avatar.png"
+                    alt="profile-image"
+                    width={65}
+                    height={54}
+                    className="object-cover"
+                />
             </div>
         </div>
     </div>

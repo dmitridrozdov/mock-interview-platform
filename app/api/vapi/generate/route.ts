cfd17819842495bbed5ff0157ca153ddeea3237a
dmitridrozdov <dm.drozdov@gmail.com>
2025-05-21 12:06:22 +1000

    Update route.ts

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -4,4 +4,11 @@ export async function GET(request: Request) {
 
 export async function POST(request: Request) {
     const { type, role, level, techstack, amount, userid } = await request.json();
+
+    try {
+        
+    } catch (error) {
+        console.error("Error:", error);
+        return Response.json({ success: false, error: error }, { status: 500 });
+      }
 }

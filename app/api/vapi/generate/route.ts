7e2a09cfc955ba9df54c0f3f0ee578b15c5e6b65
dmitridrozdov <dm.drozdov@gmail.com>
2025-05-21 13:38:09 +1000

    Update route.ts

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -1,3 +1,7 @@
+import { generateText } from "ai"
+import { google } from "@ai-sdk/google"
+import { db } from "@/firebase/admin"
+
 export async function GET(request: Request) {
     return Response.json({ success: true, data: "Thank you" }, { status: 200 });
 }
@@ -6,7 +10,7 @@ export async function POST(request: Request) {
     const { type, role, level, techstack, amount, userid } = await request.json();
 
     try {
-        
+
     } catch (error) {
         console.error("Error:", error);
         return Response.json({ success: false, error: error }, { status: 500 });
